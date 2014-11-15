5c2401d8dc415f3e5eb3a374a8d46f45c54b2d25
rbrick <rbrickproductions123@gmail.com>
2014-11-15 13:04:37 -0600

    Add player request

--- api.php
+++ api.php
@@ -1,22 +1,29 @@
 <?php
 
-require './vendor/predis/predis/src/Autoloader.php';
+include './vendor/predis/predis/src/Autoloader.php';
 
 Predis\Autoloader::register();
 
 
 // 'a' is the kind of search to perform on the database, 'q' is the query
+
+$predis = new Predis\Client();
+
+$predis->connect();
+
+$api = new API($predis);
+
 if(isset($_GET['a'], $_GET['q'])) {
     switch ($_GET['a']) {
         case 'team':
-            show_team($_GET['q']);
+            //show_team($_GET['q']);
             break;
         case 'player':
-            show_player($_GET['q']);
+            $api->respondPlayer($_GET['q']);
             break;
         default :
              $error = array(
-         "message" => "An error has occurred",
+        "cause" => "An error has occurred",
         "status" => "ERROR"
         );
         echo json_encode($error);
@@ -25,9 +32,31 @@ if(isset($_GET['a'], $_GET['q'])) {
     
 } else {
     $error = array(
-      "message" => "An error has occurred",
+      "cause" => "An error has occurred",
       "status" => "ERROR"
     );
     
     echo json_encode($error);
+}
+
+
+class API {
+   
+    private static $predis = null;
+    
+    function __construct($predis) {
+        $this->predis = $predis;
+    }
+    
+    public function respondPlayer($player) {
+        if($this->predis->hexists("players", $player) == false) {
+            $error = array("cause" => "Player does not exist!", "status" => "ERROR");
+            echo json_encode($error);
+            return;
+        } else {
+           $response = array("name" => $player, "gold" => $this->predis->hget("players", $player));
+           echo json_encode($response);
+           return;
+        }
+    }
 }
